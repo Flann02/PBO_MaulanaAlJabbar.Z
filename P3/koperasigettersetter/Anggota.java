9ce4212efd6b07a49219ddb815633130303465d0
Maulana Al Jabbar.Z <mlana.alj22@gmail.com>
2026-09-14 19:13:10 +0700

    P3_Percobaan4

--- P3/koperasigettersetter/Anggota.java
+++ P3/koperasigettersetter/Anggota.java
@@ -3,6 +3,12 @@ package P3.koperasigettersetter;
 public class Anggota {
     private String nama, alamat;
     private float simpanan;
+    
+    Anggota(String nama, String alamat){
+        this.nama = nama;
+        this.alamat = alamat;
+        this.simpanan = 0;
+    }
 
     public void setNama(String nm){
         this.nama = nm;
