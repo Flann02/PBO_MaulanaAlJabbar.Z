b7db530225472e49bc36707098e7937eb8983cc6
Maulana Al Jabbar.Z <mlana.alj22@gmail.com>
2026-09-14 19:57:17 +0700

    P3_Tugas

---
+++ P3/EncapDemo.java
@@ -0,0 +1,28 @@
+package P3;
+
+public class EncapDemo {
+    private String nama;
+    private int age;
+
+    public String getName(){
+        return nama;
+    }
+
+    public void setName(String newName){
+        nama=newName;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int newAge){
+        if (newAge > 30) {
+            age = 30;
+        }else if (newAge < 18) {
+            age = 18;
+        }else {
+            age = newAge;
+        }
+    }
+}
---
+++ P3/TestLogistik.java
@@ -0,0 +1,26 @@
+package P3;
+
+import java.util.Scanner;
+
+public class TestLogistik {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Kontainer kontainerAlfa = new Kontainer("REQ-9988", "PT. Maju Bersama", 5000);
+
+        System.out.println("Nama Pemilik Kontainer: " + kontainerAlfa.getNamaPemilik());
+        System.out.println("Kapasitas Maksimal: " + kontainerAlfa.getKapasitasMaksimal() + " kg");
+
+        System.out.print("\nMasukkan berat muatan yang ingin ditambahkan (kg): ");
+        double beratTambah = input.nextDouble();
+        kontainerAlfa.tambahMuatan(beratTambah);
+        System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+
+        System.out.print("\nMasukkan berat muatan yang ingin diturunkan (kg): ");
+        double beratTurun = input.nextDouble();
+        kontainerAlfa.turunkanMuatan(beratTurun);
+        System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+
+        input.close();
+    }
+}
